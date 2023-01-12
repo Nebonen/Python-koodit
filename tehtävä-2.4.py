8c9c1c11ec9921a524bf73f2b76c344531c6d150
Miska <miska.nurminen@gmail.com>
2023-01-12 10:34:23 +0200

    Loput 2. tehtävät

---
+++ tehtävä-2.4.py
@@ -0,0 +1,15 @@
+luku1 = input('Anna ensimmäinen luku: ')
+luku2 = input(' Anna toinen luku: ')
+luku3 = input('Anna kolmas luku: ')
+
+luku1num = int(luku1)
+luku2num = int(luku2)
+luku3num = int(luku3)
+
+summa = luku1num + luku2num + luku3num
+tulo = luku1num * luku2num * luku3num
+keskiarvo = (luku1num + luku2num + luku3num) / 3
+
+print('Summa on: ' + str(summa))
+print('Tulo on: ' + str(tulo))
+print('keskiarvo on: ' + str(keskiarvo))
---
+++ tehtävä-2.6.py
@@ -0,0 +1,12 @@
+import random
+
+kkoodi1 = random.randint(0, 9)
+kkoodi2 = random.randint(0, 9)
+kkoodi3 = random.randint(0, 9)
+print(f'Kolme numeroinen koodi on {kkoodi1}{kkoodi2}{kkoodi3}')
+
+nkoodi1 = random.randint(1, 6)
+nkoodi2 = random.randint(1, 6)
+nkoodi3 = random.randint(1, 6)
+nkoodi4 = random.randint(1, 6)
+print(f'Neli numeroinen koodi on {nkoodi1}{nkoodi2}{nkoodi3}{nkoodi4}')
